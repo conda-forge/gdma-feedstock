fdf288075dcd5a0ef08761cb41bd542043be1bc2
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-31 18:19:17 -0800

    fix UCRT migration

--- recipe/bld-py.bat
+++ recipe/bld-py.bat
@@ -1,28 +1,18 @@
-for /f %%i in ('python -c "import platform; print(int(platform.python_implementation() == 'PyPy'))"') do set IS_PYPY=%%i
-
 cmake %CMAKE_ARGS% ^
   -G "Ninja" ^
   -S "%SRC_DIR%\\pygdma" ^
-  -B "build_py%PY_VER%%IS_PYPY%" ^
+  -B "build_py%PY_VER%" ^
   -D CMAKE_BUILD_TYPE=Release ^
-  -D CMAKE_INSTALL_PREFIX="%PREFIX%" ^
-  -D CMAKE_CXX_COMPILER=clang-cl ^
   -D CMAKE_CXX_FLAGS="/EHsc %CXXFLAGS%" ^
-  -D CMAKE_INSTALL_LIBDIR="Library\lib" ^
-  -D CMAKE_INSTALL_INCLUDEDIR="Library\include" ^
-  -D CMAKE_INSTALL_BINDIR="Library\bin" ^
-  -D CMAKE_INSTALL_DATADIR="Library\share" ^
   -D PYMOD_INSTALL_LIBDIR="/../../Lib/site-packages" ^
   -D gdma_INSTALL_CMAKEDIR="Library\share\cmake\gdma" ^
   -D Python_EXECUTABLE="%PYTHON%" ^
   -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
-  -D CMAKE_VERBOSE_MAKEFILE=OFF ^
-  -D CMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"
+  -D CMAKE_VERBOSE_MAKEFILE=ON
 if errorlevel 1 exit 1
 
-cmake --build build_py%PY_VER%%IS_PYPY% ^
+cmake --build build_py%PY_VER% ^
       --config Release ^
       --target install ^
       -- -j %CPU_COUNT%
 if errorlevel 1 exit 1
-
--- recipe/build-py.sh
+++ recipe/build-py.sh
@@ -1,28 +1,15 @@
-if [ "$(uname)" == "Darwin" ]; then
-    ARCH_ARGS=""
+#!/usr/bin/env bash
 
-    # c-f-provided CMAKE_ARGS handles CMAKE_OSX_DEPLOYMENT_TARGET, CMAKE_OSX_SYSROOT
-fi
-if [ "$(uname)" == "Linux" ]; then
-    ARCH_ARGS=""
+set -o xtrace -o nounset -o pipefail -o errexit
 
-fi
-
-IS_PYPY=$(${PYTHON} -c "import platform; print(int(platform.python_implementation() == 'PyPy'))")
-
-${BUILD_PREFIX}/bin/cmake ${CMAKE_ARGS} ${ARCH_ARGS} \
+cmake ${CMAKE_ARGS} \
   -S ${SRC_DIR}/pygdma \
-  -B build_py${PY_VER}${IS_PYPY} \
+  -B build_py${PY_VER} \
   -G Ninja \
-  -D CMAKE_INSTALL_PREFIX=${PREFIX} \
   -D CMAKE_BUILD_TYPE=Release \
-  -D CMAKE_CXX_COMPILER=${CXX} \
-  -D CMAKE_CXX_FLAGS="${CXXFLAGS}" \
-  -D CMAKE_INSTALL_LIBDIR=lib \
   -D PYMOD_INSTALL_LIBDIR="/python${PY_VER}/site-packages" \
   -D Python_EXECUTABLE=${PYTHON} \
   -D CMAKE_VERBOSE_MAKEFILE=OFF \
   -D CMAKE_PREFIX_PATH="${PREFIX}"
 
-cmake --build build_py${PY_VER}${IS_PYPY} --target install -j${CPU_COUNT}
-
+cmake --build build_py${PY_VER} --target install -j${CPU_COUNT}
